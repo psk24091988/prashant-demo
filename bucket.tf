33b6865c800a5ff7a141977a4e6fce689b9a116a
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-29 00:40:01 +0530

    Create bucket.tf

---
+++ bucket.tf
@@ -0,0 +1,10 @@
+resource "google_storage_bucket" "prashant-terra-buck"  {
+  name = "prashant-terra-buck"
+  prefix = "terraform/tfstat"
+  force_destroy = false
+  location      = var.region
+  storage_class = "STANDARD"
+  versioning {
+   enabled = true
+  }
+}
