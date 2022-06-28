e1fddcfb7c28514d8e1ff1b1b5841cd06c8e3fdf
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-29 01:49:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -75,4 +75,130 @@ module "gke" {
     ]
   }
 }
+resource "google_container_node_pool" "node_pool" {
+  provider = google-beta
+
+  name     = "main-pool"
+  project  = var.project
+  location = var.location
+  
+  initial_node_count = "1"
+
+  autoscaling {
+    min_node_count = "1"
+    max_node_count = "5"
+  }
+
+  management {
+    auto_repair  = "true"
+    auto_upgrade = "true"
+  }
+
+  node_config {
+    image_type   = "COS"
+    machine_type = "n1-standard-1"
+
+    labels = {
+      all-pools-example = "true"
+    }
+
+    tags = [
+      module.vpc_network.public,
+      "helm-example",
+    ]
+
+    disk_size_gb = "30"
+    disk_type    = "pd-standard"
+    preemptible  = false
+
+    service_account = var.service_account
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [initial_node_count]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+
+module "gke_service_account" {
+  name        = var.service_account
+  project     = var.project
+  description = "this is for service account"
+}
+
+resource "random_string" "suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+module "vpc_network" {
+  name_prefix = var.vpc_name
+  project     = var.project
+  region      = var.region
+
+  cidr_block           = var.vpc_cidr_block
+  secondary_cidr_block = var.vpc_secondary_cidr_block
+
+  public_subnetwork_secondary_range_name = var.public_subnetwork_secondary_range_name
+  public_services_secondary_range_name   = var.public_services_secondary_range_name
+  public_services_secondary_cidr_block   = var.public_services_secondary_cidr_block
+  private_services_secondary_cidr_block  = var.private_services_secondary_cidr_block
+  secondary_cidr_subnetwork_width_delta  = var.secondary_cidr_subnetwork_width_delta
+  secondary_cidr_subnetwork_spacing      = var.secondary_cidr_subnetwork_spacing
+}
+
+resource "null_resource" "configure_kubectl" {
+  provisioner "local-exec" {
+    command = "gcloud beta container clusters get-credentials ${module.gke_cluster.name} --region ${var.region} --project ${var.project}"
+
+    environment = {
+      KUBECONFIG = var.kubectl_config_path != "" ? var.kubectl_config_path : ""
+    }
+  }
+
+  depends_on = [google_container_node_pool.node_pool]
+}
+
+resource "kubernetes_cluster_role_binding" "user" {
+  metadata {
+    name = "admin-user"
+  }
+
+  role_ref {
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "User"
+    name      = data.google_client_openid_userinfo.terraform_user.email
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind      = "Group"
+    name      = "system:masters"
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "helm_release" "nginx" {
+  depends_on = [google_container_node_pool.node_pool]
+
+  repository = "https://charts.bitnami.com/bitnami"
+  name       = "nginx"
+  chart      = "nginx"
+}
+
   
