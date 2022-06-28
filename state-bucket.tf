1620be97733322e40539226d4bc766ca5f1c7bd2
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 18:22:49 +0530

    Update state-bucket.tf

--- state-bucket.tf
+++ state-bucket.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "prashant-terra-buck"  {
   name = "prashant-terra-buck"
   prefix = "terraform/tfstat"
-  force_destroy = true
+  force_destroy = false
   location      = var.region
   storage_class = "STANDARD"
   versioning {
